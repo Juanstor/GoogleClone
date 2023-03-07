3d703ac3112f374fba41b453c98583e452e50aa5
Juanstor <alvarezjuansbs@gmail.com>
2023-03-07 11:10:08 -0500

    Add js interactions, make it responsive desing in 640px, great progres

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+let linkProfile = document.querySelector('.profileImage');
+let linkSuerte = document.getElementById('secondButtom');
+
+linkProfile.addEventListener('click', function() {
+    window.location.href = 'https://accounts.google.com/';
+});
+
+linkSuerte.addEventListener('click', function() {
+    window.location.href = 'https://github.com/Juanstor';
+});
